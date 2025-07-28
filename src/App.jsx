e9c7f6cf1e86ad7cf1ffac9243f5cc65a26d7d40
Temirlan Raiymbek <raiymbektemirlan25@gmail.com>
2025-07-29 00:07:59 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, accusamus voluptatum. Est unde voluptates quae voluptatem accusamus voluptatibus ad quo, repellat, velit sit error architecto. Repellat odio adipisci pariatur molestiae.</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
