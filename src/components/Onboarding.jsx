803d065559b9dd76b6d787f027c6cd2a5db0f3b4
Temirlan Raiymbek <raiymbektemirlan25@gmail.com>
2025-07-31 00:05:43 +0500

    test onboarding page4

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -5,26 +5,23 @@ import illustration from '../../public/images/onboarding.png';
 const Onboarding = ({ onComplete }) => {
   return (
     <div className="onboarding">
-      <div className="onboarding__illustration">
-        <img src={illustration} alt="Habit Tracker Illustration" />
+      <div className="onboarding__top">
+        <img src={illustration} alt="Habit Tracker Illustration" className="onboarding__img" />
       </div>
-      <div className="onboarding__text">
-        <h2>
+      <div className="onboarding__card">
+        <h2 className="onboarding__title">
           Welcome to the<br />
           Habit Tracker!
         </h2>
-        <p>
+        <p className="onboarding__desc">
           Create healthy habits and achieve<br />
           your goals with our easy-to-use<br />
           tracker.
         </p>
+        <button className="onboarding__btn" onClick={onComplete}>
+          Create a New Habit
+        </button>
       </div>
-      <button
-        onClick={onComplete}
-        className="onboarding__btn"
-      >
-        Create a New Habit
-      </button>
     </div>
   );
 };
