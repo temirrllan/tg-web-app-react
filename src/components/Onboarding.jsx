031c3380d7f6c06cb0702ec789a8cf766f7612c4
Temirlan Raiymbek <raiymbektemirlan25@gmail.com>
2025-07-31 09:01:28 +0500

    check des4

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Onboarding.css';
-import illustration from '../../public/images/onboarding.png'; // Проверь путь к картинке
+import illustration from '../../public/images/onboarding.png'; // путь проверь!
 
 const Onboarding = ({ onComplete }) => {
   return (
     <div className="onboarding">
       <div className="onboarding__top">
         <img src={illustration} alt="Habit Tracker Illustration" className="onboarding__img" />
+      </div>
+      {/* SVG-дуга для плавного перехода */}
+      <div className="onboarding__wave">
+        <svg viewBox="0 0 375 80" width="100%" height="80" preserveAspectRatio="none">
+          <path d="M0,80 Q187.5,0 375,80 L375,0 L0,0 Z" fill="#fff"/>
+        </svg>
       </div>
       <div className="onboarding__card">
         <h2 className="onboarding__title">
