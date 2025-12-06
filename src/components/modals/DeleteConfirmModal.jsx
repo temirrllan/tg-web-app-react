c63f4c511a8be323e20aa0f53d10a0baf48e5eec
Temirlan <raiymbektemirlan25@gmail.com>
2025-12-06 12:52:51 +0500

    centre DeleteConfirmModal

--- src/components/modals/DeleteConfirmModal.jsx
+++ src/components/modals/DeleteConfirmModal.jsx
@@ -14,24 +14,22 @@ const DeleteConfirmModal = ({ isOpen, onClose, onConfirm, habitTitle }) => {
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} className="delete-modal">
-      <div className="delete-modal__content">
-        <h2 className="delete-modal__title">
-          Are You Sure You Want<br />to Remove the Habit?
-        </h2>
-        <div className="delete-modal__buttons">
-          <button 
-            className="delete-modal__button delete-modal__button--yes"
-            onClick={handleYes}
-          >
-            Yes
-          </button>
-          <button 
-            className="delete-modal__button delete-modal__button--no"
-            onClick={handleNo}
-          >
-            No
-          </button>
-        </div>
+      <h2 className="delete-modal__title">
+        Are You Sure You Want<br />to Remove the Habit?
+      </h2>
+      <div className="delete-modal__buttons">
+        <button 
+          className="delete-modal__button delete-modal__button--yes"
+          onClick={handleYes}
+        >
+          Yes
+        </button>
+        <button 
+          className="delete-modal__button delete-modal__button--no"
+          onClick={handleNo}
+        >
+          No
+        </button>
       </div>
     </Modal>
   );
